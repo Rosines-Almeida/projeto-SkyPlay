c7ead185365ccd7664169eca9897fe79fe4ca339
R. da Silva Almeida <r.da.silva.almeida@avanade.com>
2020-01-23 10:54:14 -0300

    requisição da api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+ $(document).ready(function(){
+     
+
+        let movies = "";
+        const url = 'https://sky-frontend.herokuapp.com/movies';
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            success: loadMovvies,
+            error: error
+          });
+     
+         function loadMovvies(data) {
+        movies = data;
+        console.log(movies);
+      }
+function error(e){
+console.log(e)
+}
+
+
  
+})
